2a1217ed39d939294047602f745dc2aed3533c65
yy1300326388 <1300326388@qq.com>
2015-01-09 17:59:25 +0800

    增加上传文件

--- app/src/main/java/com/zhengsonglan/jobdemo/libs/androidasynchttp/AndroidAsyncHttpActivity.java
+++ app/src/main/java/com/zhengsonglan/jobdemo/libs/androidasynchttp/AndroidAsyncHttpActivity.java
@@ -6,18 +6,23 @@ import android.widget.Button;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 import com.zhengsonglan.jobdemo.R;
 import com.zhengsonglan.jobdemo.base.BaseActivity;
 import com.zhengsonglan.jobdemo.base.BaseActivityInterface;
 
 import org.apache.http.Header;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * Created by zsl on 2014/12/31.
  */
 public class AndroidAsyncHttpActivity extends BaseActivity implements BaseActivityInterface,View.OnClickListener {
 
-    Button bt_basehttp,bt_basehttp2;
+    Button bt_basehttp,bt_basehttp2,bt_basehttp3,bt_basehttp4;
 
     String url="http://www.baidu.com";
     @Override
@@ -33,12 +38,18 @@ public class AndroidAsyncHttpActivity extends BaseActivity implements BaseActivi
     public void initView() {
         bt_basehttp= (Button) findViewById(R.id.androidasynchttp_bt_basehttp);
         bt_basehttp2= (Button) findViewById(R.id.androidasynchttp_bt_basehttp2);
+        bt_basehttp3= (Button) findViewById(R.id.androidasynchttp_bt_basehttp3);
+        bt_basehttp4= (Button) findViewById(R.id.androidasynchttp_bt_basehttp4);
+
+
     }
 
     @Override
     public void initEvent() {
         bt_basehttp.setOnClickListener(this);
         bt_basehttp2.setOnClickListener(this);
+        bt_basehttp3.setOnClickListener(this);
+        bt_basehttp4.setOnClickListener(this);
     }
 
     @Override
@@ -58,10 +69,71 @@ public class AndroidAsyncHttpActivity extends BaseActivity implements BaseActivi
                 baseAsyncHttp2();
                 break;
             }
+            case R.id.androidasynchttp_bt_basehttp3:{
+                PostRequest();
+                break;
+            }
+            case R.id.androidasynchttp_bt_basehttp4:{
+                uploadFile();
+
+
+                break;
+            }
+
+
             default:{
                 break;
             }
         }
+    }
+
+    /**
+     * 上传文件
+     */
+    private void uploadFile() {
+        try {
+            RequestParams params=new RequestParams();
+            params.put("name","test");
+            params.put("name",new File("test/aa.jpg"));
+            AsyncHttpClient httpClient=new AsyncHttpClient();
+            httpClient.post(this,url,params,new FileAsyncHttpResponseHandler(this) {
+
+                @Override
+                public void onFailure(int i, Header[] headers, Throwable throwable, File file) {
+                    showToast("failure");
+                }
+
+                @Override
+                public void onSuccess(int i, Header[] headers, File file) {
+                    showToast("success");
+                }
+
+            });
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            showToast("file not selected");
+        }
+    }
+
+    /**
+     * post请求
+     */
+    private void PostRequest() {
+        RequestParams requestParams=new RequestParams();
+        requestParams.put("name","test");
+        requestParams.put("pwd","123");
+        AsyncHttpClient client=new AsyncHttpClient();
+        client.post(this,url,requestParams,new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                showToast("成功");
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                showToast("失败");
+            }
+        });
     }
 
     /**
@@ -109,4 +181,6 @@ public class AndroidAsyncHttpActivity extends BaseActivity implements BaseActivi
             }
         });
     }
+
+
 }
